cb485dc959de3dd8b19f9f89f1c4f766554d6dd8
nikito1993 <120242077+nikito1993@users.noreply.github.com>
2023-02-28 08:15:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml;
+
+namespace _01_Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, HashSet<char>> Words = new Dictionary<string, HashSet<char>>
+            {
+                {
+                    "pear",new HashSet<char>()
+                },
+                {
+                    "pork", new HashSet<char>()
+                },
+                {
+                   "olive" , new HashSet<char>()
+                },
+                {
+                    "flour", new HashSet<char>()
+                }
+            };
+
+            Queue<char> vowels = new Queue<char>(string.Join("",Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries)));
+
+            Stack<char> consonants = new Stack<char>(string.Join("", Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)));
+
+            while (consonants.Count>0)
+            {
+                char vowel = vowels.Dequeue();
+                char consonat = consonants.Pop();
+
+                foreach (var word in Words)
+                {
+                    if (word.Key.Contains(vowel))
+                    {
+                        word.Value.Add(vowel);
+                    }
+                    if (word.Key.Contains(consonat))
+                    {
+                        word.Value.Add(consonat);
+                    }
+                }
+                vowels.Enqueue(vowel);
+            }
+            List<string>WordsFinder=Words.Where(w=>w.Key.Count()==w.Value.Count).Select(w=>w.Key).ToList();
+
+            Console.WriteLine($"Words found: {WordsFinder.Count}");
+            Console.WriteLine(string.Join(Environment.NewLine,WordsFinder));
+        }
+    }
+}
